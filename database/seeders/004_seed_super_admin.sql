407a88b0bbfba8e31906489959b86939641c6980
Adi <yos.evony2022@gmail.com>
2026-06-26 16:23:18 +0700

    [S1-T12] feat: reserve super admin seeder

---
+++ database/seeders/004_seed_super_admin.sql
@@ -0,0 +1,16 @@
+-- =====================================================
+-- Seeder : 004_seed_super_admin.sql
+-- Description : Reserved for Super Admin JavaScript Seeder
+-- =====================================================
+
+-- Super Admin dibuat melalui:
+-- lib/super-admin-seeder.js
+--
+-- Alasan:
+-- - Password harus di-hash.
+-- - Menggunakan Environment Variable.
+-- - Tidak menyimpan password di repository.
+--
+-- File ini sengaja dikosongkan agar urutan seeder tetap konsisten.
+
+SELECT 1;
